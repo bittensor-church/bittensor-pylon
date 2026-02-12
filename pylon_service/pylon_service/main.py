77075d19438f8ab0acc8da8beb423ce7b341759e
Kordian Kowalski <kordian.kowalski@reef.pl>
2026-02-12 18:34:38 +0100

    feat: latest block info
    And endpoint and the corresponding client methods for querying the
    latest block number (and hash). For now using litestar cache on the
    server side, as this endpoint requires fresher data than others and
    using the cache seems to be a safe and simple solution.
    
    Issue: BACT-269
    Impacts: service, client

--- pylon_service/pylon_service/main.py
+++ pylon_service/pylon_service/main.py
@@ -12,7 +12,7 @@ from pylon_service.request_id import RequestIdMiddleware
 from pylon_service.routers import v1_router
 from pylon_service.schema import PylonSchemaPlugin
 from pylon_service.sentry_config import init_sentry
-from pylon_service.settings import settings
+from pylon_service.settings import response_cache_config, settings
 from pylon_service.stores import stores
 
 
@@ -41,6 +41,7 @@ def create_app() -> Litestar:
         plugins=[PylonSchemaPlugin()],
         exception_handlers={ArchiveFallbackException: archive_fallback_handler},
         stores=stores,
+        response_cache_config=response_cache_config,
         debug=settings.debug,
         logging_config=litestar_logging_config(),
     )
