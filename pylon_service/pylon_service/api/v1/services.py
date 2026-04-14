afd5d1cb588cc5b8f578f3581d7715b2f07eb36d
Michał Nowacki <michal.nowacki@reef.pl>
2026-04-14 15:51:56 +0200

    commitment filtering logic in v1

--- pylon_service/pylon_service/api/v1/services.py
+++ pylon_service/pylon_service/api/v1/services.py
@@ -1,4 +1,4 @@
-from pylon_commons.models import CommitmentVariant
+from pylon_commons.models import CommitmentVariant, CommitmentKind
 from pylon_commons.types import Hotkey, NetUid
 from pylon_commons.v1.responses import GetCommitmentResponse, GetCommitmentsResponse
 
@@ -6,6 +6,7 @@ from pylon_service.api._unstable.services import BlockService, CertificateServic
 from pylon_service.bittensor.contact import BittensorPort
 from pylon_service.bittensor.models import Block
 from pylon_service.services.commitments import CommitmentService as DomainCommitmentService
+from pylon_service.services.errors import CommitmentNotFoundError
 
 
 class CommitmentService:
@@ -17,17 +18,21 @@ class CommitmentService:
         commitments = await self._domain.get_commitments(contact_router, netuid, block)
         return GetCommitmentsResponse(
             block=commitments.block,
-            commitments={hotkey: commitment.commitment for hotkey, commitment in commitments.commitments.items()},
+            commitments={hotkey: c.commitment for hotkey, c in c.commitments.items() if c.kind == CommitmentKind.HEX_DATA},
         )
 
     async def get_commitment(
         self, contact_router: BittensorPort, netuid: NetUid, hotkey: Hotkey
     ) -> GetCommitmentResponse:
         block, commitment = await self._domain.get_commitment(contact_router, netuid, hotkey)
+        if commitment.kind != CommitmentKind.HEX_DATA:
+            raise CommitmentNotFoundError()
         return self._wrap_commitment(block, commitment)
 
     async def get_own_commitment(self, contact_router: BittensorPort, netuid: NetUid) -> GetCommitmentResponse:
         block, commitment = await self._domain.get_own_commitment(contact_router, netuid)
+        if commitment.kind != CommitmentKind.HEX_DATA:
+            raise CommitmentNotFoundError()
         return self._wrap_commitment(block, commitment)
 
     @staticmethod
